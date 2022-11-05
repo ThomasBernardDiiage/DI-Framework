96d6fa298cc8a94662a358262481b8444d56e7ba
Nicolas Richet <nicolas.richet@diiage.org>
2022-11-05 10:09:07 +0100

    Ajout du scope avec tests

---
+++ DI-Framework/DI-Framework.Tests/Sut/Services/ClassWithoutInterface.cs
@@ -0,0 +1,19 @@
+﻿using DI_Framework.Tests.Sut.Interfaces;
+
+namespace DI_Framework.Tests.Sut.Services
+{
+    internal class ClassWithoutInterface
+    {
+        private readonly IRandomGuidProvider _randomGuidProvider;
+
+        public ClassWithoutInterface(IRandomGuidProvider randomGuidProvider)
+        {
+            _randomGuidProvider = randomGuidProvider;
+        }
+
+        public Guid GetGuid()
+        {
+            return _randomGuidProvider.RandomGuid;
+        }
+    }
+}

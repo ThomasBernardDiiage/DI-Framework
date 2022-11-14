7206aa875a078f39010dfdf73495f5fd90a5db38
Thomas <thomas.bernard@diiage.org>
2022-11-14 11:41:06 +0100

    Test imbicates services + lifescope

--- DI-Framework/DI-Framework.Tests/ContainerTest.cs
+++ DI-Framework/DI-Framework.Tests/ContainerTest.cs
@@ -6,35 +6,82 @@ namespace DI_Framework.Tests;
 
 public class ContainerTest
 {
+    [Fact] 
+    public void TestResolveUnregisteredService()
+    {
+        var services = new DiServiceCollection();
+
+        var container = services.GenerateContainer(); // Generate container without register our service
+        
+        Assert.Throws<NotRegisteredException>(() => container.GetService<GuidService>()); // Must throw an Exception
+    }
+
+
+
     [Fact]
-         public void TestResolveUnregisteredService()
-         {
-             var services = new DiServiceCollection();
-                 
-             var container = services.GenerateContainer(); // Generate container without register our service
-             
-             Assert.Throws<NotRegisteredException>(() => container.GetService<GuidService>()); // Must throw an Exception
-         }
-         
-         
-         private abstract class SutAbstractClass
-         {
-         }
-         
-         private interface SutInterface
-         {
-         }
-         
-         //[Fact]
-         //public void TestAbstractOrInterface()
-         //{
-         //    var services = new DiServiceCollection();
-        //
-         //    Assert.Throws<AbstractOrInterfaceClassException>(() => services.RegisterSingleton<SutAbstractClass>()); // Must throw an Exception (we can't register abstract classes)
-          //   Assert.Throws<AbstractOrInterfaceClassException>(() => services.RegisterSingleton<SutInterface>()); // Must throw exception if we use an Interface
-             
-          //   services.GenerateContainer(); // Generate container without register our service
-         //}
+    public void TestImbricateService()
+    {
+        var services = new DiServiceCollection();
+        services.RegisterSingleton<FirstClass>();
+        services.RegisterSingleton<ISecondClass, SecondClass>();
+        services.RegisterSingleton<ThirdClass>();
+
+
+        var container = services.GenerateContainer();
+
+        var instanceFirstClass = container.GetService<FirstClass>();
+        Assert.NotNull(instanceFirstClass?.SecondClass?.ThirdClass);
+    }
     
-    
+    [Fact]
+    public void TestImbricateServiceSingleton()
+    {
+        var services = new DiServiceCollection();
+        services.RegisterTransient<FirstClass>();
+        services.RegisterSingleton<ISecondClass, SecondClass>();
+        services.RegisterTransient<ThirdClass>();
+
+        
+        var container = services.GenerateContainer();
+
+        var instance1 = container.GetService<FirstClass>(); // Get 2 different instance of FirstClass
+        var instance2 = container.GetService<FirstClass>(); // Get 2 different instance of FirstClass
+        
+        Assert.NotEqual(instance1, instance2); // Transient so must be different instances
+        Assert.Equal(instance1?.SecondClass, instance2?.SecondClass); // Singleton so must be the same instance
+    }
+}
+
+public class FirstClass
+{
+    public Guid Guid => System.Guid.NewGuid();
+    public ISecondClass SecondClass { get; }
+    public FirstClass(ISecondClass secondClass)
+    {
+        SecondClass = secondClass;
+    }
+}
+
+public class SecondClass : ISecondClass
+{
+    public Guid Guid => System.Guid.NewGuid();
+    public ThirdClass ThirdClass { get; }
+    public SecondClass(ThirdClass thridClass)
+    {
+        ThirdClass = thridClass;
+    }
+}
+
+public interface ISecondClass
+{
+    Guid Guid { get; }
+    ThirdClass ThirdClass { get; }
+}
+
+public class ThirdClass
+{
+    public Guid Guid = System.Guid.NewGuid();
+    public ThirdClass()
+    {
+    }
 }
